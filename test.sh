472727e143ce1c6d804a92db81bcca512f2fd1d2
IshaqMd <55760349+ishaq-md@users.noreply.github.com>
2026-03-28 00:10:18 +0530

    Reduce sleep duration in test.sh

--- test.sh
+++ test.sh
@@ -67,7 +67,7 @@ cat temp.json | tr -d \\n > request.json
 # Instruction before sending the API request
 echo
 echo "${CYAN_TEXT}${BOLD_TEXT}Step 5:${RESET_FORMAT} ${GREEN_TEXT}Sending the request to the Document AI API. This might take some time.${RESET_FORMAT}"
-sleep 25
+sleep 20
 export LOCATION="us"
 export PROJECT_ID=$(gcloud config get-value core/project)
 curl -X POST \
@@ -79,7 +79,7 @@ https://${LOCATION}-documentai.googleapis.com/v1beta3/projects/${PROJECT_ID}/loc
 # Instruction before displaying the output
 echo
 echo "${CYAN_TEXT}${BOLD_TEXT}Step 6:${RESET_FORMAT} ${GREEN_TEXT}Displaying the processed document text.${RESET_FORMAT}"
-sleep 20
+sleep 15
 cat output.json | jq -r ".document.text"
 
 # Instruction before downloading the Python script
