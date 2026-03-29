84fddae2b5cb2f55b404a90537a864038045e727
IshaqMd <55760349+ishaq-md@users.noreply.github.com>
2026-03-29 13:04:44 +0530

    Refactor test.sh by cleaning up output and timingsfrf
    Removed unnecessary echo statements and adjusted sleep durations.

--- test.sh
+++ test.sh
@@ -15,10 +15,11 @@ BOLD_TEXT=$'\033[1m'
 UNDERLINE_TEXT=$'\033[4m'
 clear # Clear the terminal screen
 
+export PROJECT_ID=$(gcloud config get-value project)
 gcloud services enable documentai.googleapis.com
 export ZONE=$(gcloud compute instances list document-ai-dev --format 'csv[no-heading](zone)')
 gcloud compute ssh document-ai-dev --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
-export PROJECT_ID=$(gcloud config get-value project)
+sleep 5
 curl -X POST \
   -H "Authorization: Bearer $(gcloud auth print-access-token)" \
   -H "Content-Type: application/json" \
@@ -28,16 +29,6 @@ curl -X POST \
   }' \
   "https://us-documentai.googleapis.com/v1/projects/${PROJECT_ID}/locations/us/processors"
 
-echo
-echo "${BLUE_TEXT}${BOLD_TEXT}╔══════════════════════════════════════╗${RESET_FORMAT}"
-echo "${BLUE_TEXT}${BOLD_TEXT}║   WELCOME TO DR ABHISHEK CLOUD      ║${RESET_FORMAT}"
-echo "${BLUE_TEXT}${BOLD_TEXT}╚══════════════════════════════════════╝${RESET_FORMAT}"
-echo
-
-# Instruction for entering the Processor ID
-echo
-echo "${CYAN_TEXT}${BOLD_TEXT}Please enter your Processor ID:${RESET_FORMAT}"
-#read -r PROCESSOR_ID
 echo "My Processor ID is: $PROCESSOR_ID"
 export PROCESSOR_ID
 
@@ -54,8 +45,6 @@ echo "${CYAN_TEXT}${BOLD_TEXT}Step 2:${RESET_FORMAT} ${GREEN_TEXT}Creating a ser
 export PROJECT_ID=$(gcloud config get-value core/project)
 export SA_NAME="document-ai-service-account"
 gcloud iam service-accounts create $SA_NAME --display-name $SA_NAME
-
-
 
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
 --member="serviceAccount:$SA_NAME@${PROJECT_ID}.iam.gserviceaccount.com" \
@@ -83,7 +72,7 @@ cat temp.json | tr -d \\n > request.json
 # Instruction before sending the API request
 echo
 echo "${CYAN_TEXT}${BOLD_TEXT}Step 5:${RESET_FORMAT} ${GREEN_TEXT}Sending the request to the Document AI API. This might take some time.${RESET_FORMAT}"
-sleep 20
+sleep 15
 export LOCATION="us"
 export PROJECT_ID=$(gcloud config get-value core/project)
 curl -X POST \
@@ -95,7 +84,7 @@ https://${LOCATION}-documentai.googleapis.com/v1beta3/projects/${PROJECT_ID}/loc
 # Instruction before displaying the output
 echo
 echo "${CYAN_TEXT}${BOLD_TEXT}Step 6:${RESET_FORMAT} ${GREEN_TEXT}Displaying the processed document text.${RESET_FORMAT}"
-sleep 20
+sleep 15
 cat output.json | jq -r ".document.text"
 
 # Instruction before downloading the Python script
@@ -130,8 +119,3 @@ curl -X POST \
 -H "Content-Type: application/json; charset=utf-8" \
 -d @request.json \
 https://${LOCATION}-documentai.googleapis.com/v1beta3/projects/${PROJECT_ID}/locations/${LOCATION}/processors/${PROCESSOR_ID}:process > output.json
-
-# Final message
-echo
-echo "${BLUE_TEXT}${BOLD_TEXT}Subscribe to Dr Abhishek Cloud Tutorial:${RESET_FORMAT} ${MAGENTA_TEXT}${UNDERLINE_TEXT}https://www.youtube.com/@drabhishek.5460/videos${RESET_FORMAT}"
-echo
