51011d5f74f631c4ec4d02d7fb265470b89460d1
IshaqMd <55760349+ishaq-md@users.noreply.github.com>
2026-03-29 17:18:46 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -51,7 +51,7 @@ gsutil cp gs://cloud-training/gsp924/health-intake-form.pdf .
 # Instruction before creating the JSON request
 echo
 echo "${CYAN_TEXT}${BOLD_TEXT}Step 4:${RESET_FORMAT} ${GREEN_TEXT}Preparing the JSON request for Document AI API.${RESET_FORMAT}"
-sleep 20
+sleep 18
 echo '{"inlineDocument": {"mimeType": "application/pdf","content": "' > temp.json
 base64 health-intake-form.pdf >> temp.json
 echo '"}}' >> temp.json
